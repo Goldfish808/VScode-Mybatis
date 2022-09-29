d1a01d0176a3ebe727d79412062cf32e930463c0
Goldfish808 <tjddns0853@gmail.com>
2022-09-29 16:47:34 +0900

    Product DAO 완료

--- src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
+++ src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
@@ -1,5 +1,15 @@
 package site.metacoding.firstapp.domain.product;
 
-public interface ProductDao {
+import java.util.List;
 
+public interface ProductDao {
+  public Product findById(Integer productId);
+
+  public List<Product> findAll();
+
+  public void insert(Product product);
+
+  public void update(Integer id, Product prodcut);
+
+  public void deleteById(Integer id);
 }
