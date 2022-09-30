3b413f690883c92ab7e831269f2e881f1afc2f28
Goldfish808 <tjddns0853@gmail.com>
2022-09-30 11:19:00 +0900

    [Junit CRUD 체크 완료]

--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -1,12 +1,16 @@
 package site.metacoding.firstapp.domain.product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
+
 import site.metacoding.firstapp.config.MyBatisConfig;
 
 @Import(MyBatisConfig.class) // MyBatisTest가 MyBatisConfig를 못읽음
@@ -30,4 +34,69 @@ public class ProductDaoTest {
     assertEquals("바나나", productPS.getProductName());
 
   }
+
+  @Test
+  public void findAll_test() {
+    // given
+
+    // when
+    List<Product> productPS = productDao.findAll();
+
+    // then
+    assertEquals(2, productPS.size());
+
+  }
+
+  @Test
+  public void insert_test() {
+    String name = "수박";
+    Integer price = 1800;
+    Integer qty = 88;
+
+    // given
+    Product product = new Product(name, price, qty);
+    // when
+    int result = productDao.insert(product);
+    // then
+    assertEquals(1, result);
+  }
+
+  @Test
+  public void update_test() {
+    // given
+    Integer productId = 1;
+    String name = "수박";
+    Integer price = 1800;
+    Integer qty = 88;
+
+    Product product = new Product(name, price, qty); // 업데이트 할 내용
+    product.setProductId(productId);
+
+    // verify : Update 경우에, 영속화( DB에 있는지 체크 ) 시켜야함
+    Product productPS = productDao.findById(productId);
+    assertTrue(productPS == null ? false : true);
+
+    // when : Update 실제 테스트
+    product.update(product);
+    int result = productDao.update(productPS);
+
+    // then
+    assertEquals(1, result);
+  }
+
+  @Test
+  public void delete_test() {
+    // given
+    Integer productId = 1;
+
+    Product productPS = productDao.findById(productId);
+    assertTrue(productPS == null ? false : true);
+
+    // when
+    int result = productDao.deleteById(productId);
+
+    // then
+    assertEquals(1, result);
+  }
+
 }
